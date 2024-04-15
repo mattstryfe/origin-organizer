d73bbf1da9880909739da55f1dcb5d60e1b59b26
Matthew Esterberg <purebias@gmail.com>
2024-04-14 21:00:10 -0400

    firebase things

---
+++ src/plugins/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBuKE7bhrh3mdeNlZ_kry-zZiYkcA5UTOE",
+  authDomain: "origin-organizer.firebaseapp.com",
+  projectId: "origin-organizer",
+  storageBucket: "origin-organizer.appspot.com",
+  messagingSenderId: "54163027071",
+  appId: "1:54163027071:web:1c2e729a5e4d269a75bb4a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
