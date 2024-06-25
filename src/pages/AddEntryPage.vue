1bdc9eead1f7c89d8892f0eb4d140c735e6092d4
Matthew Esterberg <purebias@gmail.com>
2024-06-25 19:06:13 -0400

    Tg 13 form schema store (#7)
    * TG-13
    
    Expanded waffle menu.  added some small 'wow-factor' css behavior.  added routing. OO icon now takes to home page.
    
    * TG-13
    
    Add entry base schema and form is mostly done.  Breed placeholders have been converted to chips to infer multiples.  type of entry selection has been modified to a v btn group AND data/choice is also being appended to live reactive object
    
    * Update entryFormStore.js
    
    * cleanup
    
    cleanup
    
    * TG-13 #in-pr
    
    Added custom form type picker, notes, dynamic characteristics schema, and expanded on some of the styling and iteration.  Added some helpful comments and left some extra code in there as examples for future.
    
    * cleanup

--- src/pages/AddEntryPage.vue
+++ src/pages/AddEntryPage.vue
@@ -1,16 +1,67 @@
 <template>
   <v-row dense no-gutters>
-    <v-col>
-      <h1> Add Entry Page</h1>
+    <v-col cols="12" class="mb-4">
+      <v-card border class="pb-2">
+        <v-card-title>Add Entry</v-card-title>
+        <v-card-subtitle>Advanced Add/Expansion</v-card-subtitle>
+        <v-card-subtitle>Add multiple?</v-card-subtitle>
+        <v-card-subtitle>How many?</v-card-subtitle>
+        <v-card-subtitle>Display them and edit manually?</v-card-subtitle>
+      </v-card>
+    </v-col>
+    <v-divider class="mb-2"></v-divider>
+    <v-col class="v-col-6">
+      <!-- Custom Entry Type Picker -->
+      <v-btn-toggle
+        v-for="(opt, index) in schemaPhaseOptions"
+        :key="index"
+        v-model="formData['type']"
+        class="my-2"
+      >
+        <v-btn class="mr-1 border" :value="opt.type">
+          <v-icon v-for="i in opt.icon" :key="i" :size="opt.iconSize">
+            {{ i }}
+          </v-icon>
+        </v-btn>
+      </v-btn-toggle>
+
+      <!-- basic form -->
+      <add-entry-form></add-entry-form>
+
+      <!-- some placeholder info -->
+      <v-card class="pb-2">
+        <v-card-title>Power User Tools</v-card-title>
+        <v-card-subtitle>Add Another?</v-card-subtitle>
+        <v-card-subtitle>Add Multiple?</v-card-subtitle>
+        <v-card-subtitle>Save as template?</v-card-subtitle>
+      </v-card>
+    </v-col>
+
+    <!-- Right side widget to hold Object for DEV-ing -->
+    <v-col class="ml-3 v-col-5">
+      <v-card min-height="1000" class="overflow-auto">
+        <v-card-title> Live Reactive/Object</v-card-title>
+        <v-card-text>
+          <pre>
+            {{ formData }}
+          </pre>
+        </v-card-text>
+      </v-card>
     </v-col>
   </v-row>
 </template>
 
-
 <script setup>
-
+import AddEntryForm from '@/components/AddEntryForm.vue'
+import { storeToRefs } from 'pinia'
+import { useEntryFormStore } from '@/stores/entryFormStore'
+const entryFormStore = useEntryFormStore()
+const { formData } = storeToRefs(entryFormStore)
+import { schemaPhaseOptions } from '@/entryFormSchema'
 </script>
 
 <style scoped>
-
+pre {
+  white-space: pre-wrap;
+}
 </style>
---
+++ src/stores/entryFormStore.js
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+
+export const useEntryFormStore = defineStore('entryFormStore', {
+  state: () => ({
+    formData: {}
+  }),
+
+  getters: {
+
+  },
+
+  actions: {
+    updateField(field, value) {
+      this.formData[field] = value
+    }
+  }
+})
