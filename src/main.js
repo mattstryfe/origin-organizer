f0bb9129c15cb044e8c4fa658436f6e601426b8c
Matthew Esterberg <purebias@gmail.com>
2024-08-25 19:08:12 -0400

    TG-32 #in-pr test user and auto login is done (#11)
    Test user auto logs in if you are in mode.dev locally. can be toggled off by commenting out main.js entry.  Tried to make this as minimally invasive as possible.

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import router from '@/plugins/router.js'
 
 import './assets/css/style.css'
 import { vuetify } from '@/plugins/vuetify'
-
+import { useUserStore } from '@/stores/userStore'
 
 createApp(App)
   .use(pinia)
   .use(router)
   .use(vuetify)
   .mount('#app')
+
+if (import.meta.env.MODE === 'development') {
+  const userStore = useUserStore()
+  userStore.handleLogin(true)
+}
