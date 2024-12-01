4ca4823351a9cf363aa833f5a5db88ff0354ead4
Matthew Esterberg <purebias@gmail.com>
2024-12-01 17:03:34 -0500

    Added ability to remove entries
    2 follow on tickets have been generated to make this better.  TG-48 and TG-49.  Both aim to improve user experience.

--- src/components/Cards/DisplayEntryCard.vue
+++ src/components/Cards/DisplayEntryCard.vue
@@ -2,7 +2,7 @@
   <v-card
     :width="smAndDown ? 210 : 300"
     :height="smAndDown ? 300 : 400"
-    class="border-sm ma-1 pa-1"
+    class="border-sm ma-1 pa-1 d-flex flex-column"
     :class="{
       'opacity-80': showOverlay
     }"
@@ -32,7 +32,7 @@
       :height="smAndDown ? 100 : 150"
       src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
       cover
-      class="text-black align-end mb-1"
+      class="text-black align-end"
     >
       <v-rating
         density="compact"
@@ -43,36 +43,58 @@
       ></v-rating>
     </v-img>
 
-    <v-divider class="my-2"></v-divider>
-
     <!-- Breed area -->
-    <v-row v-if="allEntryDetails?.breed" no-gutters>
+    <v-row v-if="allEntryDetails?.breed" no-gutters dense>
       <v-col cols="5" class="text-right pr-2">
         {{ allEntryDetails.breed[0] }}
       </v-col>
       <template v-if="allEntryDetails?.breed?.length > 1">
         <v-col>
-          <v-icon size="xx-large">mdi-compare-horizontal</v-icon>
+          <v-icon size="large">mdi-compare-horizontal</v-icon>
         </v-col>
         <v-col cols="5" class="text-left">
           {{ allEntryDetails.breed[1] }}
         </v-col>
       </template>
     </v-row>
-    <v-card-subtitle>Name: {{ allEntryDetails.name }}</v-card-subtitle>
-    <v-card-subtitle>Age: {{ allEntryDetails.DoB }}</v-card-subtitle>
+
+    <v-row dense no-gutters>
+      <v-col>
+        <v-card-subtitle>Name: {{ allEntryDetails.name }}</v-card-subtitle>
+        <v-card-subtitle>Age: {{ allEntryDetails.DoB }}</v-card-subtitle>
+      </v-col>
+    </v-row>
 
     <v-divider class="my-1"></v-divider>
-    <v-row no-gutters class="px-2 mt-2">
-      <v-chip
-        v-for="c in allEntryDetails.characteristics"
-        :key="c"
-        class="mr-1 text-grey lighten-2 mb-1"
-        variant="outlined"
-        size="x-small"
-      >
-        {{ c }}
-      </v-chip>
+
+    <v-row dense no-gutters>
+      <v-col class="overflow-y-auto" style="max-height: 60px">
+        <v-chip
+          v-for="c in allEntryDetails.characteristics"
+          :key="c"
+          class="mr-1 text-grey lighten-2 mb-1"
+          variant="outlined"
+          size="x-small"
+        >
+          {{ c }}
+        </v-chip>
+      </v-col>
+    </v-row>
+
+    <v-spacer class=""></v-spacer>
+
+    <!-- bottom Controls -->
+    <v-row dense no-gutters class=" ">
+      <v-col class="d-flex justify-end">
+        <v-btn
+          @click="entryFormStore.removeThisEntry(entryId)"
+          icon="mdi-delete-outline"
+          size="medium"
+          variant="text"
+          density="compact"
+          color="blue-darken-2"
+        ></v-btn>
+      </v-col>
     </v-row>
   </v-card>
 </template>
