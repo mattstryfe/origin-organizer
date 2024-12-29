9cd7eaae1f7e0dc1f97a483ae773330ca42d0fbe
Matthew Esterberg <purebias@gmail.com>
2024-12-29 17:46:35 -0500

    cleanup

--- src/main.js
+++ src/main.js
@@ -2,14 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import pinia from '@/plugins/pinia.js'
 import router from '@/plugins/router.js'
-
 import './assets/css/style.css'
 import { vuetify } from '@/plugins/vuetify'
-// import { useUserStore } from '@/stores/userStore'
 
 createApp(App).use(pinia).use(router).use(vuetify).mount('#app')
-
-// if (import.meta.env.MODE === 'development') {
-//   const userStore = useUserStore()
-//   userStore.handleLogin(true)
-// }
