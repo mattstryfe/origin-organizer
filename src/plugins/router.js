9ba4ebeba70e6197f6ee396b83cece5a9d4702dd
Matthew Esterberg <purebias@gmail.com>
2025-03-12 10:31:41 -0400

    Update router.js
    cleanup

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,35 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AddEntryPage from '@/pages/AddEntryPage.vue'
-import LandingPage from '@/pages/LandingPage.vue'
-import FlockManagerPage from '@/pages/FlockManagerPage.vue'
-import EntryDetailsPage from '@/pages/EntryDetailsPage.vue'
 import { useEntryFormStore } from '@/stores/entryFormStore'
 import { routes } from '@/schemas/routerLinksSchema'
-// const routes = [
-//   {
-//     name: 'LandingPage',
-//     path: '/',
-//     component: LandingPage
-//   },
-//   { name: 'AddEntry', path: '/addentry', component: AddEntryPage },
-//   { name: 'FlockManager', path: '/flockmanager', component: FlockManagerPage },
-//   {
-//     name: 'EntryDetails',
-//     path: '/entrydetails/:entryId',
-//     component: EntryDetailsPage,
-//     props: true
-//   }
-// ]
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
+
 // keep it simple for now.
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: createWebHistory(),
+  history: createWebHistory()
   routes
-  // routes // short for `routes: routes`
 })
-console.log('router', router)
 
 router.beforeEach(async (to, from) => {
   // If not authed
